a1c4f21cb66c4975a141ceb8c186974c5abcb596
dongha <vandong9@gmail.com>
2023-05-21 02:48:45 +0700

    update header view

--- lib/presentation/widget/loading/header/vib_header.dart
+++ lib/presentation/widget/loading/header/vib_header.dart
@@ -1,10 +1,18 @@
 import 'dart:convert';
 
 import 'package:flutter/material.dart';
+import 'package:share_ui/theme/app_theme.dart';
+import 'package:share_ui/theme/theme_manager.dart';
+
+import '../../../../r.dart';
 
 class VIBHeaderView extends StatelessWidget {
-  String leftIconName = "";
+  String title = "Mở tài khoản";
+  String leftIconName = R.controlsImages.cmBackIcImages.cmBackIc;
+  String rightIconName =
+      R.controlsImages.cmCloseHeaderIcImagesetImages.cmCloseHeaderIc;
 
+  Color backgroundColor = Color.fromRGBO(0, 0, 0, 0);
   void loadJson() {
     String json = _jsonexport;
     Map<String, dynamic> jsonOBject = jsonDecode(json);
@@ -13,14 +21,29 @@ class VIBHeaderView extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    InheritedAppThemeProvider themeProvider =
+        InheritedAppThemeProvider.of(context)!; // Little tricky force
+    AppTheme currentTheme = themeProvider.appTheme;
+
     return Container(
+      color: backgroundColor,
+      padding: EdgeInsets.only(left: 18, right: 18),
       child: Row(
+        mainAxisAlignment: MainAxisAlignment.spaceBetween,
         children: [
           Container(
-            width: 40,
-            height: 40,
-            padding: EdgeInsets.all(8),
-            child: Icon(Icons.abc_outlined),
+            width: 30,
+            height: 30,
+            child: Image.asset(leftIconName),
+          ),
+          Text(
+            title,
+            style: currentTheme.textStyle.headline(currentTheme.color.grey900),
+          ),
+          Container(
+            width: 30,
+            height: 30,
+            child: Image.asset(rightIconName),
           )
         ],
       ),
--- lib/presentation/widget/loading/simulator/simulator.dart
+++ lib/presentation/widget/loading/simulator/simulator.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:my_bad/presentation/widget/loading/header/vib_header.dart';
 import 'package:my_bad/presentation/widget/loading/status_bar/status_bard.dart';
+import 'package:share_ui/theme/app_theme.dart';
+import 'package:share_ui/theme/theme_manager.dart';
 
 class SimulatorWidget extends StatefulWidget {
   @override
@@ -10,15 +12,26 @@ class SimulatorWidget extends StatefulWidget {
 }
 
 class _SimulatorWidgetState extends State<SimulatorWidget> {
+  Color backgroundColor = Colors.white;
+
   @override
   Widget build(BuildContext context) {
+    InheritedAppThemeProvider themeProvider =
+        InheritedAppThemeProvider.of(context)!; // Little tricky force
+    AppTheme currentTheme = themeProvider.appTheme;
+    backgroundColor = currentTheme.color.grey50;
+
     return Container(
-      width: 375,
-      height: 812,
-      decoration: BoxDecoration(
-          color: Colors.white,
-          borderRadius: BorderRadius.all(Radius.circular(38))),
-      child: Column(children: [StatusBardWidget(), VIBHeaderView()]),
+      color: Colors.red,
+      padding: EdgeInsets.all(16),
+      child: Container(
+        width: 375,
+        height: 812,
+        decoration: BoxDecoration(
+            color: backgroundColor,
+            borderRadius: BorderRadius.all(Radius.circular(38))),
+        child: Column(children: [StatusBardWidget(), VIBHeaderView()]),
+      ),
     );
   }
 }
